07d02354915cb33923feecbf053588750fb14c0e
zozo89 <v.prinko@yandex.ru>
2017-03-12 14:53:37 +0200

    finish progect width discoball

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,17 @@
-var ball = document.querySelector(".discoball");
-
+var myBall = document.querySelector(".discoball");
+var discoClasses = ["here", "goes", "our", "awesome", "disco", "ball"];
+var i = 0;
 function goDisco() {
-	//do your disco magic here !
-}
+	// console.log(i);
+		myBall.setAttribute("class", "discoball " + discoClasses[i]);
+		i++;
+		if( i >= 6 ){
+			i = 0;
+		}
+	};
+
 
 setInterval(goDisco,300);
+
+
+// .here .goes .our .awesome .disco .ball
